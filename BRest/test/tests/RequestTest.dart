c67a4bcb2ebdad6b4149172cfd9cab239e123bff
Brandon Donnelson <branflake2267@gmail.com>
2012-07-29 15:38:44 -0700

    Working on the rest tests

--- BRest/test/tests/RequestTest.dart
+++ BRest/test/tests/RequestTest.dart
@@ -1,10 +1,11 @@
 #import('/Users/branflake2267/workspace-sdk/dart-sdk/lib/unittest/unittest.dart');
-#import('../rest/Request.dart');
+#import('../../rest/Request.dart');
 
 // TODO how to test html methods
 void main() {
   String url = "http://google.com";
-  //Request request = new Request.send(url, (e) => test);
+  Request request = new Request.openGet(url, (e) => test);
+  request.send();
   
   
 }
